5ec5acfa5222b38861df078f9d468060b673d92c
nrframe <72948882+nrframe@users.noreply.github.com>
2021-01-23 02:21:44 -0500

    Add files via upload

---
+++ Scripts/TrackingManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class TrackingManager : MonoBehaviour
+{
+    private ARTrackedImageManager myTrackedImageManager;
+
+    [SerializeField] private GameObject lighthouseObject;
+
+    private GameObject placedLighthouse;
+
+    private int counter = 0;
+
+    private void Awake()
+    {
+         myTrackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    private void Update()
+    {
+
+            if (myTrackedImageManager.trackables.count == 1 && counter == 0)
+            {
+            PlaceLighthouse();
+            }
+    }
+
+    public void PlaceLighthouse()
+    {
+        foreach(var trackedImage in myTrackedImageManager.trackables)
+        {
+            placedLighthouse = Instantiate(lighthouseObject, trackedImage.transform.position, Quaternion.identity);
+        }
+        counter++;
+    }
+
+    public void ResetTracking()
+    {
+        counter = 0;
+    }
+}
+
+
---
+++ Scripts/UIManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("TextBoxes")]
+
+    public Text findSurfaceText;
+
+    private List<Text> listText = new List<Text>();
+
+    [Header("Buttons")]
+
+    public Button resetTrackingButton;
+
+    public Button looksGoodButton;
+
+    public Button confirmLGButton;
+
+    public Button denyLGButton;
+
+    private List<Button> listButtons = new List<Button>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        listText.Add(findSurfaceText);
+
+        
+        listButtons.Add(resetTrackingButton);
+        listButtons.Add(looksGoodButton);
+        listButtons.Add(confirmLGButton);
+        listButtons.Add(denyLGButton);
+        
+    }
+
+    //called by looksGoodButton
+    public void PromptForConfirmation()
+    {
+        findSurfaceText.text = "This is a good spot for the lighthouse?";
+
+    }
+
+    //called by confirmLGButton
+    public void TransitionScene(string tagOfDesireScene)
+    {
+
+        foreach (Text currentText in listText)
+        {
+            if (currentText.CompareTag(tagOfDesireScene))
+                currentText.gameObject.SetActive(true);
+
+            else
+                currentText.gameObject.SetActive(false);
+        }
+
+        foreach (Button currentButton in listButtons)
+        {
+            if (currentButton.CompareTag(tagOfDesireScene))
+                currentButton.gameObject.SetActive(true);
+
+            else
+                currentButton.gameObject.SetActive(false);
+        }
+
+        
+    }
+}
